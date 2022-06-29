018ef68b15c238bd4b22e9d757df5215321e5b6f
Trey Wenger <tvwenger@gmail.com>
2022-06-29 09:32:05 -0700

    cleanup

--- galstruct/run_comparisons.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Starting Comparisons
-python compare_models.py data/varysims_maf data/varysims_maf --variedparam nsims
-python compare_models.py data/varysims_nsf data/varysims_nsf --variedparam nsims 
-python compare_models.py data/varyfeatures_maf data/varyfeatures_maf --variedparam feat  
-python compare_models.py data/varyfeatures_nsf data/varyfeatures_nsf --variedparam feat  
-python compare_models.py data/varylayers_maf data/varylayers_maf --variedparam layers  
-python compare_models.py data/varylayers_nsf data/varylayers_nsf --variedparam layers 
