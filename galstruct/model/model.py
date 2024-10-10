8156de5b0247b1329a6291f11b34c173b3b5b254
Jakob Mills <jakobmills27@gmail.com>
2024-10-10 11:27:17 -0500

    Update model to class (#3)
    * Update model to class
    
    * Updated model to class structure (2nd attempt)

--- galstruct/model/model.py
+++ galstruct/model/model.py
@@ -25,193 +25,154 @@ Trey Wenger - August 2020
 Trey Wenger - May 2022 - Formatting
 """
 
+#import some necessary python stuff
 import torch as tt
+import numpy as np
+import matplotlib.pyplot as plt
 
-from . import rotcurve
-from . import transforms
+#updated script import statements, these pull from files in the same directory
+#from . import rotcurve
+#from . import transforms
+import rotcurve
+import transforms 
 
-
-def model_vlsr(
-    dist,
-    cos_glong,
-    sin_glong,
-    cos_glat,
-    sin_glat,
-    R0,
-    Usun,
-    Vsun,
-    Wsun,
-    Upec,
-    Vpec,
-    cos_tilt,
-    sin_tilt,
-    cos_roll,
-    sin_roll,
-    R0a22,
-    lam,
-    loglam,
-    term1,
-    term2,
-    theta0,
-):
+class Model:
     """
-    Derive the model-predicted IAU-LSR velocity at a given position.
+    This class generates a model galaxy, with default parameters provided.
+    """     
 
-    Inputs:
-      dist :: scalar (kpc)
-        Distance
-      cos_glong, sin_glong, cos_glat, sin_glat :: scalars
-        Consine and sine of Galactic longitude and latitude
-      R0 :: scalar (kpc)
-        Solar Galactocentric radius
-      Usun, Vsun, Wsun :: scalars (km/s)
-        Solar peculiar motion components
-      Upec, Vpec :: scalars (km/s)
-        Peculiar motion components
-      cos_tilt, sin_tilt, cos_roll, sin_roll :: scalars
-        Cosine and sine of Galactic plane tilt and roll angles
-      R0a22, lam, loglam, term1, term2 :: scalars
-        The rotation curve constants
-      theta0 :: scalar (km/s)
-        Rotation speed at R0
-
-    Returns: vlsr
-      vlsr :: scalar (km/s)
-        IAU-LSR velocity
-    """
-    # Convert distance to R, azimuth
-    midplane_dist = dist * cos_glat
-    R = tt.sqrt(R0 ** 2.0 + midplane_dist ** 2.0 - 2.0 * R0 * midplane_dist * cos_glong)
-    cos_az = (R0 - midplane_dist * cos_glong) / R
-    sin_az = midplane_dist * sin_glong / R
-
-    # Calculate rotation speed at R
-    theta = rotcurve.calc_theta(R, R0a22, lam, loglam, term1, term2)
-    vR = -Upec
-    vAz = theta + Vpec
-
-    # Convert velocities to barycentric Cartesian frame
-    vXg, vYg = transforms.v_gcencyl_to_gcencar(vR, vAz, cos_az, sin_az)
-    vXb, vYb, vZb = transforms.v_gcencar_to_barycar(
-        vXg, vYg, Usun, Vsun, Wsun, theta0, cos_tilt, sin_tilt, cos_roll, sin_roll
-    )
-
-    # Convert velocities to IAU-LSR radial velocity
-    vlsr = transforms.calc_vlsr(cos_glong, sin_glong, cos_glat, sin_glat, vXb, vYb, vZb)
-    return vlsr
-
-
-def model(
-    az,
-    az0,
-    tan_pitch,
-    R0,
-    Usun,
-    Vsun,
-    Wsun,
-    Upec,
-    Vpec,
-    cos_tilt,
-    sin_tilt,
-    cos_roll,
-    sin_roll,
-    R0a22,
-    lam,
-    loglam,
-    term1,
-    term2,
-    theta0,
-    warp_amp,
-    warp_off,
-    Rref=8.0,
-):
-    """
-    Derive the model-predicted Galactic longitude, latitude,
-    IAU-LSR velocity, velocity gradient w.r.t. distance, and distance
-    of a spiral at a given azimuth.
-
-    Inputs:
-      az :: scalar (radians)
-        Galactocentric azimuth
-      az0 :: scalar (radians)
-        Reference azimuth at Rref
-      tan_pitch :: scalar
-        Tangent of pitch angle
-      R0 :: scalar (kpc)
-        Galactocentric radius of the Sun
-      Usun, Vsun, Wsun :: scalars (km/s)
-        Solar peculiar motion components
-      Upec, Vpec :: scalars (km/s)
-        Peculiar motion components
-      cos_tilt, sin_tilt, cos_roll, sin_roll :: scalars
-        Cosine and sine of Galactic plane tilt and roll angles
-      R0a22, lam, loglam, term1, term2 :: scalars
-        The rotation curve constants
-      theta0 :: scalar (km/s)
-        Rotation speed at R0
-      warp_amp, warp_off :: scalars (kpc-1, rad)
-        Warp is defined as Z(R, az) = warp_amp*(R - R_ref)^2 * sin(az - warp_off)
-      Rref :: scalar (kpc)
-        Reference Galactocentric radius
-
-    Returns: glong, glat, vlsr, dvlsr_ddist, dist
-      glong, glat :: scalars (radians)
-        Galactic longitude and latitude
-      vlsr :: scalar (km/s)
-        IAU-LSR velocity
-      dvlsr_ddist :: scalar
-        Partial derivative of IAU-LSR velocity w.r.t. distance
-      dist :: scalar (kpc)
-        Distance
-    """
-    R = Rref * tt.exp((az0 - az) * tan_pitch)
-    cos_az = tt.cos(az)
-    sin_az = tt.sin(az)
-
-    # Apply Galactic warp
-    Zg = warp_amp * (R - Rref) ** 2.0 * tt.sin(az - warp_off)
-    Zg[R < Rref] = 0.0
-
-    # Convert positions to Galactocentric Cartesian frame
-    Xg, Yg = transforms.gcencyl_to_gcencar(R, cos_az, sin_az)
-
-    # Convert positions to the barycentric Cartesian frame
-    Xb, Yb, Zb = transforms.gcencar_to_barycar(
-        Xg, Yg, Zg, R0, cos_tilt, sin_tilt, cos_roll, sin_roll
-    )
-
-    # Convert positions to Galactic longitude and latitude
-    cos_glong, sin_glong, cos_glat, sin_glat, dist = transforms.barycar_to_galactic(
-        Xb, Yb, Zb
-    )
-    glong = tt.atan2(sin_glong, cos_glong)
-    glat = tt.asin(sin_glat)
-
-    # Get IAU-LSR velocity and derivative w.r.t. distance
-    dist.requires_grad = True
-    vlsr = model_vlsr(
+    #init helps us to define some default model parameters
+    def __init__(self,
+                az0 = tt.tensor(np.pi), 
+                tan_pitch = tt.tensor(np.tan(np.deg2rad(14))), 
+                R0 = tt.tensor(8.5), 
+                Usun = tt.tensor(0.0), 
+                Vsun = tt.tensor(0.0), 
+                Wsun = tt.tensor(0.0), 
+                Upec = tt.tensor(0.0), 
+                Vpec = tt.tensor(0.0), 
+                sin_tilt = tt.tensor(0.0), 
+                cos_tilt = tt.tensor(1.0), 
+                cos_roll = tt.tensor(1.0), 
+                sin_roll = tt.tensor(0.0), 
+                a2 = tt.tensor(0.95), 
+                a3= tt.tensor(1.65), 
+                theta0 = tt.tensor(220.0), 
+                warp_amp = tt.tensor([0.0]), 
+                warp_off = tt.tensor([0.0]), 
+                Rref = tt.tensor(8.0)):                    
+        self.az0 = az0
+        self.tan_pitch = tan_pitch
+        self.R0 = R0
+        self.Usun = self.Vsun = self.Wsun = Usun = Vsun = Wsun
+        self.Upec = self.Vpec = Upec = Vpec
+        self.sin_tilt = sin_tilt
+        self.cos_tilt = cos_tilt
+        self.cos_roll= cos_roll
+        self.sin_roll = sin_roll
+        self.a2 = a2 #a2 and a3 are needed for the rotcurve_constraints function to obtain R0a22, lam, loglam, term1, and term2
+        self.a3 = a3
+        self.R0a22, self.lam, self.loglam, self.term1, self.term2 = rotcurve.rotcurve_constants(self.R0, self.a2, self.a3)
+        self.theta0 = theta0
+        self.warp_amp = warp_amp
+        self.warp_off = warp_off
+        self.Rref = Rref
+    
+    def model_vlsr(self,
         dist,
         cos_glong,
         sin_glong,
         cos_glat,
         sin_glat,
-        R0,
-        Usun,
-        Vsun,
-        Wsun,
-        Upec,
-        Vpec,
-        cos_tilt,
-        sin_tilt,
-        cos_roll,
-        sin_roll,
-        R0a22,
-        lam,
-        loglam,
-        term1,
-        term2,
-        theta0,
-    )
-    vlsr.sum().backward()
-    dvlsr_ddist = dist.grad
-    return glong, glat, vlsr, dvlsr_ddist, dist
+    ):
+        """
+        Derive the model-predicted IAU-LSR velocity at a given position.
+    
+        Inputs:
+          dist :: scalar (kpc)
+            Distance
+          cos_glong, sin_glong, cos_glat, sin_glat :: scalars
+            Consine and sine of Galactic longitude and latitude
+    
+        Returns: vlsr
+          vlsr :: scalar (km/s)
+            IAU-LSR velocity
+        """
+        # Convert distance to R, azimuth
+        midplane_dist = dist * cos_glat
+        R = tt.sqrt(self.R0 ** 2.0 + midplane_dist ** 2.0 - 2.0 * self.R0 * midplane_dist * cos_glong)
+        cos_az = (self.R0 - midplane_dist * cos_glong) / R
+        sin_az = midplane_dist * sin_glong / R
+    
+        # Calculate rotation speed at R
+        theta = rotcurve.calc_theta(R, self.R0a22, self.lam, self.loglam, self.term1, self.term2)
+        vR = -self.Upec
+        vAz = theta + self.Vpec
+    
+        # Convert velocities to barycentric Cartesian frame
+        vXg, vYg = transforms.v_gcencyl_to_gcencar(vR, vAz, cos_az, sin_az)
+        vXb, vYb, vZb = transforms.v_gcencar_to_barycar(
+            vXg, vYg, self.Usun, self.Vsun, self.Wsun, self.theta0, self.cos_tilt, self.sin_tilt, self.cos_roll, self.sin_roll
+        )
+    
+        # Convert velocities to IAU-LSR radial velocity
+        vlsr = transforms.calc_vlsr(cos_glong, sin_glong, cos_glat, sin_glat, vXb, vYb, vZb)
+        return vlsr
+    
+    
+    def model(self, az):
+        """
+        Derive the model-predicted Galactic longitude, latitude,
+        IAU-LSR velocity, velocity gradient w.r.t. distance, and distance
+        of a spiral at a given azimuth.
+    
+        Inputs:
+          az :: scalar (radians)
+            Galactocentric azimuth
+    
+        Returns: glong, glat, vlsr, dvlsr_ddist, dist
+          glong, glat :: scalars (radians)
+            Galactic longitude and latitude
+          vlsr :: scalar (km/s)
+            IAU-LSR velocity
+          dvlsr_ddist :: scalar
+            Partial derivative of IAU-LSR velocity w.r.t. distance
+          dist :: scalar (kpc)
+            Distance
+        """
+        R = self.Rref * tt.exp((self.az0 - az) * self.tan_pitch)
+        cos_az = tt.cos(az)
+        sin_az = tt.sin(az)
+    
+        # Apply Galactic warp
+        Zg = self.warp_amp * (R - self.Rref) ** 2.0 * tt.sin(az - self.warp_off)
+        Zg[R < self.Rref] = 0.0
+    
+        # Convert positions to Galactocentric Cartesian frame
+        Xg, Yg = transforms.gcencyl_to_gcencar(R, cos_az, sin_az)
+    
+        # Convert positions to the barycentric Cartesian frame
+        Xb, Yb, Zb = transforms.gcencar_to_barycar(
+            Xg, Yg, Zg, self.R0, self.cos_tilt, self.sin_tilt, self.cos_roll, self.sin_roll
+        )
+
+        # Convert positions to Galactic longitude and latitude
+        cos_glong, sin_glong, cos_glat, sin_glat, dist = transforms.barycar_to_galactic(
+            Xb, Yb, Zb
+        )
+        
+        glong = tt.atan2(sin_glong,cos_glong)
+        glat = tt.asin(sin_glat)
+    
+        # Get IAU-LSR velocity and derivative w.r.t. distance
+        dist.requires_grad = True
+        vlsr = self.model_vlsr(
+            dist,
+            cos_glong,
+            sin_glong,
+            cos_glat,
+            sin_glat,
+        )
+        vlsr.sum().backward()
+        dvlsr_ddist = dist.grad
+        return glong, glat, vlsr, dvlsr_ddist, dist
