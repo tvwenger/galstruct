2d1c0e9d0344a5535e5c9e3099e6cd69821cff55
Trey Wenger <tvwenger@gmail.com>
2024-12-03 15:53:34 -0600

    update slurm

--- slurm/submit_nn.sh
+++ slurm/submit_nn.sh
@@ -4,7 +4,7 @@
 #SBATCH --output="logs/%x.%j.%N.out"
 #SBATCH --error="logs/%x.%j.%N.err"
 #SBATCH --ntasks=4
-#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
 #SBATCH --export=ALL
 #SBATCH --time 72:00:00
 
@@ -13,7 +13,7 @@ conda activate galstruct
 
 srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
     --error logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.err \
-    --exclusive -N1 -n1 --mem=0 \
+    --exclusive -N1 -n1 \
     python galstruct/learn_likelihood.py \
     -n 10_000 \
     --density_estimator maf \
@@ -25,7 +25,7 @@ srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
 
 srun --output logs/%x.%j.%N.nn_100k_maf_50f_5l_50t.out \
     --error logs/%x.%j.%N.nn_100k_maf_50f_5l_50t.err \
-    --exclusive -N1 -n1 --mem=0 \
+    --exclusive -N1 -n1  \
     python galstruct/learn_likelihood.py \
     -n 100_000 \
     --density_estimator maf \
@@ -37,7 +37,7 @@ srun --output logs/%x.%j.%N.nn_100k_maf_50f_5l_50t.out \
 
 srun --output logs/%x.%j.%N.nn_1m_maf_50f_5l_50t.out \
     --error logs/%x.%j.%N.nn_1m_maf_50f_5l_50t.err \
-    --exclusive -N1 -n1 --mem=0 \
+    --exclusive -N1 -n1  \
     python galstruct/learn_likelihood.py \
     -n 1_000_000 \
     --density_estimator maf \
@@ -49,7 +49,7 @@ srun --output logs/%x.%j.%N.nn_1m_maf_50f_5l_50t.out \
 
 srun srun --output logs/%x.%j.%N.nn_10m_maf_50f_5l_50t.out \
     --error logs/%x.%j.%N.nn_10m_maf_50f_5l_50t.err \
-    --exclusive -N1 -n1 --mem=0 \
+    --exclusive -N1 -n1 \
     python galstruct/learn_likelihood.py \
     -n 10_000_000 \
     --density_estimator maf \
