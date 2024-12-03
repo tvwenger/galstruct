cbe53e380c63e0f0573de7b24393fae3318e907d
Trey Wenger <tvwenger@gmail.com>
2024-12-03 14:20:57 -0600

    update slurm

--- slurm/submit_nn.sh
+++ slurm/submit_nn.sh
@@ -3,8 +3,6 @@
 #SBATCH --job-name="train_nn"
 #SBATCH --output="logs/%x.%j.%N.out"
 #SBATCH --error="logs/%x.%j.%N.err"
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=twenger2@wisc.edu
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=8
 #SBATCH --export=ALL
@@ -13,42 +11,46 @@
 eval "$(conda shell.bash hook)"
 conda activate galstruct
 
-srun python galstruct/learn_likelihood.py \
+srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
+    --error logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.err \
+    python galstruct/learn_likelihood.py \
     -n 10_000 \
     --density_estimator maf \
     --features 50 \
     --layers 5 \
-    --sim_batch_size 1 \
     --training_batch_size 50 \
     --overwrite \
     nn_10k_maf_50f_5l_50t.pkl &
 
-srun python galstruct/learn_likelihood.py \
+srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
+    --error logs/%x.%j.%N.nn_100k_maf_50f_5l_50t.err \
+    python galstruct/learn_likelihood.py \
     -n 100_000 \
     --density_estimator maf \
     --features 50 \
     --layers 5 \
-    --sim_batch_size 1 \
     --training_batch_size 50 \
     --overwrite \
     nn_100k_maf_50f_5l_50t.pkl & 
 
-srun python galstruct/learn_likelihood.py \
+srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
+    --error logs/%x.%j.%N.nn_1m_maf_50f_5l_50t.err \
+    python galstruct/learn_likelihood.py \
     -n 1_000_000 \
     --density_estimator maf \
     --features 50 \
     --layers 5 \
-    --sim_batch_size 1 \
     --training_batch_size 50 \
     --overwrite \
     nn_1m_maf_50f_5l_50t.pkl &
 
-srun python galstruct/learn_likelihood.py \
+srun srun --output logs/%x.%j.%N.nn_10k_maf_50f_5l_50t.out \
+    --error logs/%x.%j.%N.nn_10m_maf_50f_5l_50t.err \
+    python galstruct/learn_likelihood.py \
     -n 10_000_000 \
     --density_estimator maf \
     --features 50 \
     --layers 5 \
-    --sim_batch_size 1 \
     --training_batch_size 50 \
     --overwrite \
     nn_10m_maf_50f_5l_50t.pkl &
