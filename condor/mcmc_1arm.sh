36f09fa2f632694928338a4dc792bc94e8d5e753
Trey Wenger <tvwenger@gmail.com>
2024-12-06 18:21:00 -0600

    updates

---
+++ condor/mcmc_1arm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+python mcmc_posterior.py \
+    synthetic \
+    mcmc.pkl \
+    nn_nsf_2_097_152n_64f_8l_1_024t.pkl \
+    -n 1_000 \
+    --chains 12 \
+    --ntune 1_000 \
+    --ninit 100_000 \
+    --target_accept 0.85 \
+    --num_data 200 \
+    --spiral_params \
+    1.0 \  # q
+    1.26 \ # az0
+    0.24 \ # pitch
+    5.0 \  # sigmaV
+    0.5 \  # sigma_arm_plane
+    0.1 \  # sigma_arm_height
+    --num_spirals 1 \
+    --fixed q 1.0 \
+    --fixed Zsun 5.5 \
+    --fixed roll 0.0
--- galstruct/mcmc_posterior.py
+++ galstruct/mcmc_posterior.py
@@ -100,10 +100,10 @@ _RMIN = 3.0
 _RMAX = 15.0
 _RREF = 8.0
 _NUM_SPIRALS = 4
-_NITER = 1000
-_NTUNE = 1000
-_NINIT = 100000
-_NUM_CHAINS = 4
+_NITER = 1_000
+_NTUNE = 1_000
+_NINIT = 100_000
+_NUM_CHAINS = 8
 _TARGET_ACCEPT = 0.9
 _FIXED = {}
 _OUTLIERS = None
@@ -638,7 +638,7 @@ if __name__ == "__main__":
     PARSER.add_argument(
         "-f",
         "--fixed",
-        nargs="+",
+        nargs=2,
         default=[],
         help=("Fixed GRM parameter names followed by their fixed value."),
     )
