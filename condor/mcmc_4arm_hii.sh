81f3f34f0f7de7d02ff67c36a7f4a21ac6475f75
Trey Wenger <tvwenger@gmail.com>
2025-02-03 13:46:45 -0600

    update condor

--- condor/mcmc_4arm_hii.sh
+++ condor/mcmc_4arm_hii.sh
@@ -5,10 +5,10 @@ PYTENSOR_FLAGS="base_compiledir=$tmpdir" python mcmc_posterior.py \
     hii_data.csv \
     mcmc.pkl \
     $1 \
-    -n 1_000 \
+    -n 500 \
     --chains 12 \
-    --ntune 1_000 \
-    --ninit 100_000 \
-    --target_accept 0.85 \
+    --ntune 500 \
+    --ninit 10_000 \
+    --target_accept 0.80 \
     --fixed Zsun 5.5 \
     --fixed roll 0.0
